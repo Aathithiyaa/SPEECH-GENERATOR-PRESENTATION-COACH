90326d9ad74cea6d279c31659aafcad2885c515b
Aadhithya Prakash <aadhithyasubha2018@gmail.com>
2025-04-18 21:44:15 +0530

    new commit

--- presentation.py
+++ presentation.py
@@ -1,16 +1,10 @@
 import speech_recognition as sr
-
-# Initialize recognizer
 recognizer = sr.Recognizer()
-
-# Use the microphone as the audio source
 with sr.Microphone() as source:
     print("Please speak something...")
-    # Adjust for ambient noise and record the speech
     recognizer.adjust_for_ambient_noise(source)
     audio = recognizer.listen(source)
 
-# Recognize speech using Google's speech recognition service
 try:
     print("You said: " + recognizer.recognize_google(audio))
 except sr.UnknownValueError:
